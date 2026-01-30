df395e52be6ece6fb35251e1e05ffa4e2cdced31
Samuel Guy <120762122+Ngocksam@users.noreply.github.com>
2026-01-30 13:27:17 +0100

    Add files via upload

---
+++ app.js
@@ -0,0 +1,9 @@
+const http = require('http');
+const server = http.createServer((req, res) => {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
+  res.end('Hello ! Mon application est maintenant DevOpsifiee sur Kubernetes avec ArgoCD !\n');
+});
+server.listen(3000, '0.0.0.0', () => {
+  console.log('Serveur démarré sur le port 3000');
+});
